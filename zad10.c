f93d4a79a21454fdf509191b69b3a3ae82153b73
Rafał Prokopowicz <rprokopowicz@linux-p110-10.inf.ug.edu.pl>
2018-01-21 13:12:31 +0100

    13:12

---
+++ zad10.c
@@ -0,0 +1,24 @@
+//ZAD 9
+// -lm
+// potrzebne sa tylko 2 punkty naprzeciw siebie
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct osoba{
+  int wiek;
+  int pensja;
+}osoba[n];
+
+int main(int argc, char const *argv[]) {
+  int* ludzie[5];
+  osoba osoba1;
+  for (size_t i = 0; i < 5; i++) {
+    ludzie[1] = osoba1;
+  }
+
+
+
+}
--- zad9.c
+++ zad9.c
@@ -1,30 +1,47 @@
-//ZAD 8
+//ZAD 9
 // -lm
+// potrzebne sa tylko 2 punkty naprzeciw siebie
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
 
-typedef struct position{
+typedef struct punkt{
   int x;
   int y;
-}position;
+}punkt;
+
+typedef struct prostokat{
+  punkt a;
+  punkt b;
+  punkt c;
+  punkt d;
+}prostokat;
 
 int main(int argc, char const *argv[]) {
-  position p;
-  position q;
-  p.x = 1;
-  p.y = 3;
-  q.x = 4;
-  q.y = 6;
+  prostokat prostokat1;
+  prostokat1.a.x = 1;
+  prostokat1.a.y = 1;
 
-  int odlx = abs(p.x - q.x);
-  int odly = abs(p.y - q.y);
+  prostokat1.b.x = 1;
+  prostokat1.b.y = 3;
+
+  prostokat1.c.x = 4;
+  prostokat1.c.y = 3;
+
+  prostokat1.d.x = 4;
+  prostokat1.d.y = 1;
+
+  int odlx = abs(prostokat1.a.x - prostokat1.c.x);
+  int odly = abs(prostokat1.a.y - prostokat1.c.y);
   printf("%d\n", odlx);
   printf("%d\n", odly);
 
   double odl = sqrt((odlx*odlx)+(odly*odly));
 
-  printf("%lf\n", odl);
+  double pole = odlx*odly;
+  double obwod = 2*odlx+2*odly;
+
+  printf("pole = %lf | obwod = %lf\n", pole, obwod);
 }
