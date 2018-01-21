18dcee3295e2312afebb0a7343f448272e5b302e
Rafał Prokopowicz <rprokopowicz@linux-p110-10.inf.ug.edu.pl>
2018-01-21 13:56:26 +0100

    14:00

--- zad10.c
+++ zad10.c
@@ -7,18 +7,35 @@
 #include <string.h>
 #include <time.h>
 
+#define n 5
+
 typedef struct osoba{
   int wiek;
   int pensja;
-}osoba[n];
+}osoba;
 
 int main(int argc, char const *argv[]) {
-  int* ludzie[5];
-  osoba osoba1;
+  srand(time(0));
+  int spensja = 0;
+  int swiek = 0;
+
+  //int *ludzie = malloc(5*sizeof(int));
+
+  struct osoba osoby[n];
+
+  //random wartosci
   for (size_t i = 0; i < 5; i++) {
-    ludzie[1] = osoba1;
+    osoby[i].wiek = 1 + rand() % 100;
+    osoby[i].pensja = 1 + rand() % 10000;
   }
 
+  for (size_t j = 0; j < 5; j++) {
+    printf("%d %d\n", osoby[j].wiek, osoby[j].pensja);
+    swiek += osoby[j].wiek;
+    spensja += osoby[j].pensja;
+  }
 
+  printf("sredni wiek = %d | srednia pensja = %d\n", swiek/5, spensja/5);
 
+  //free(osoby);
 }
--- zad7.c
+++ zad7.c
@@ -1,17 +1,18 @@
 //ZAD 7
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
 
-typedef struct truefalse{
-  char *a[];
-  char *b[];
-}truefalse;
+struct Bool{
+    int true;
+    int false;
+}Bool;
 
 int main(int argc, char const *argv[]) {
-  truefalse x;
-  x.a = {"true"};
-  x.b= {"false"};
+  struct Bool bool;
+  bool = {1,0};
+
 }
